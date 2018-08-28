b2ed9c516da7f6b11cfaefbf17969820fa04d32d
Shaza98 <mansourshaza@yahoo.com>
2018-08-28 20:39:35 +0200

    Initial commit

---
+++ First/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from import_export import resources
+from .models import Person
+
+class PersonResource(resources.ModelResource):
+    class Meta:
+        model = Person
